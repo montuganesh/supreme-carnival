cc9a085b3b810c621dfdaa2a37c0d08224408af5
Collin Brahana <collinbrahana@gmail.com>
2021-04-29 14:05:17 -0700

    Added a way to get satelite time, API Drafts

--- GPS_Reading.py
+++ GPS_Reading.py
@@ -24,22 +24,22 @@ def init_GPS(lHOST="127.0.0.1",PORT=None,timeout_seconds=360):
     return 0
 
 def getSingleLocation():
-    latitude = gpsd.lat()
-    longitude = gpsd.lon()
-    error_dict = gpsd.error()
-    return {"LAT":latitude,"LON":longitude,"LAT_ERROR":error_dict['y'],"LON_ERROR":error_dict["x"]} 
-    #I think error_dict["x"] and y may be bugged. Check the documentation file in the gpsd library 
-    # for what it should be, and get ready to fix.
+    return gpsd.posistion()
+    # TODO We are not using the error data provided. Determine if useful, and if so implement.
 
 
 def goFindLocation():
     #We know the gps unit won't be moving, thankfully. So I can take the median measurement.
-    # I'm not entirely happy with this, it's too vulnerable to error. Put it on the re-engineer list. 
-    lat_list = []
-    lon_list = []
+    # I'm not entirely happy with this, it's too vulnerable to error. Put it on the re-engineer list. -CB
+    LAT_list = []
+    LON_list = []
     for x in range(100):
         temphold = getSingleLocation()
-        lat_list.append(temphold["LAT"])
-        lon_list.append(temphold["LON"])
+        time.sleep(1)
+        LAT_list.append(temphold[0])
+        LON_list.append(temphold[1])
         del temphold
     return (statistics.median(lat_list),statistics.median(lon_list))
+
+def getGPStime():
+    return gpsd.time_utc()
