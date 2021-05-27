f8f66381093ab65c8be9c4d8741b00f80ef2f0a1
socrossley <socrossley@ucsb.edu>
2021-05-27 14:18:02 -0700

    Dumb typo

--- test-code/motorTest.py
+++ test-code/motorTest.py
@@ -1,6 +1,8 @@
+# import sys
+# sys.path.insert(1, 'API')
 import Motor
 from Hardware_Config import azMotorCfg as az_cfg
-from Harware_Config import altMotorCfg as alt_cfg
+from Hardware_Config import altMotorCfg as alt_cfg
 
 az_motor = Motor.Motor(az_cfg)
 alt_motor = Motor.Motor(alt_cfg)
